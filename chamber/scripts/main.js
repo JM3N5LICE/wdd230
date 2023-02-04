a2961b3ae3e0dbc03f3b15aef8654c9ac2ff1ffa
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2023-02-04 00:43:43 -0700

    button format

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -8,3 +8,7 @@ function toggleMenu(){
 
 document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
 
+var messagedate = new Date();
+if (messagedate.getDay()==1 || messagedate.getDay()==2 || messagedate.getDay()==6){
+    document.querySelector("#meet-greet").classList.add('active')
+}
