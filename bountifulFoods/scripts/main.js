c13575f2dd2ef2c7ba55dc717b40193d4559f307
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2023-03-28 10:45:51 -0600

    started hamburger menu

---
+++ bountifulFoods/scripts/main.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+    document.querySelector("#navbar").classList.toggle('menu-active');
+    document.querySelector("#menu-closed").classList.toggle('menu-active');
+    document.querySelector("#menu-open").classList.toggle('menu-active');
+}
+
+// document.querySelector("#hamburger-menu").onclick = toggleMenu;
+
+document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
+
+var messagedate = new Date();
+if (messagedate.getDay()==1 || messagedate.getDay()==2){
+    document.querySelector("#meet-greet").classList.add('active')
+}
