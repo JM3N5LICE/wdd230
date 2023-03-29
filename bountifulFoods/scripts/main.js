9a64661699f0c7023370bbe6603d176bf0e51069
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2023-03-28 23:30:56 -0600

    header

--- bountifulFoods/scripts/main.js
+++ bountifulFoods/scripts/main.js
@@ -1,14 +1,7 @@
-function toggleMenu(){
-    document.querySelector("#navbar").classList.toggle('menu-active');
-    document.querySelector("#menu-closed").classList.toggle('menu-active');
-    document.querySelector("#menu-open").classList.toggle('menu-active');
-}
-
-// document.querySelector("#hamburger-menu").onclick = toggleMenu;
-
-document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
-
-var messagedate = new Date();
-if (messagedate.getDay()==1 || messagedate.getDay()==2){
-    document.querySelector("#meet-greet").classList.add('active')
-}
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
