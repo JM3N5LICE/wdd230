52c7c1fdccaa37d391fef184b3a6b853711708e6
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2023-03-31 02:15:46 -0600

    Updated forecast

--- bountifulFoods/scripts/weather.js
+++ bountifulFoods/scripts/weather.js
@@ -1,71 +1,90 @@
 const city = "Carlsbad";
 const country = "US";
 const apiKey = "b5b67c95f3c88ae42c280e43466094ec";
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${apiKey}&units=imperial`;
-
+const currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=imperial`;
+const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${apiKey}&units=imperial`;
 
 function getWeatherData() {
-  fetch(apiUrl)
+  fetch(currentApiUrl)
     .then((response) => {
       if (response.ok) {
         return response.json();
       }
       throw new Error("Network response was not ok.");
     })
-    .then((data) => {
+    .then((currentData) => {
       // Extract current weather data
-      const currentWeatherData = data.list[0];
-      const currentTemperature = Math.round(currentWeatherData.main.temp_max);
-      const currentDescription = currentWeatherData.weather[0].description;
-      const currentHumidity = currentWeatherData.main.humidity;
-      const currentDate = new Date(data.list[0].dt * 1000);
-      console.log(currentDate.toDateString());
+      const currentDate = new Date(currentData.dt * 1000);
       
+      fetch(forecastApiUrl)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Network response was not ok.");
+        })
+        .then((forecastData) => {
+          // Extract forecast data for next three days
+          const nextThreeDays = forecastData.list.filter((item) => {
+            const itemDate = new Date(item.dt * 1000);
+            const timeDifference = itemDate.getTime() - currentDate.getTime();
+            const dayDifference = timeDifference / (1000 * 3600 * 24);
+            return dayDifference >= 0 && dayDifference <= 3;
+          });
+          
+          // Get highest temperature, condition description, humidity, and icon for each day
+          const highestTemperatures = {};
+          for (const item of nextThreeDays) {
+            const itemDate = new Date(item.dt * 1000);
+            const dayOfWeek = itemDate.toLocaleDateString("en-US", { weekday: "short" });
+            const temperature = Math.round(item.main.temp);
+            const condition = item.weather[0].description;
+            const humidity = item.main.humidity;
+            const iconUrl = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
 
-      // Extract 3 day forecast data
-      const forecastData = data.list.filter((item) => item.dt_txt.includes("12:00:00")).slice(1, 3);
-      const forecastMarkup = forecastData
-        .map((item) => {
-          const dayOfWeek = new Date(item.dt * 1000).toLocaleDateString("en-US", { weekday: "short"});
-          console.log(dayOfWeek);
-          const temperature = Math.round(item.main.temp);
-          const description = item.weather[0].description;
-          const humidity = item.main.humidity;
+            const hour = itemDate.getHours();
+            const currentDay = highestTemperatures[dayOfWeek] || { temperature: -Infinity };
 
-          const iconUrl = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
-          return `
-            <div class="forecast-day">
-              <h3>${dayOfWeek}</h3>
-              <img src="${iconUrl}" alt="weather icon">
-              <p>${temperature} &deg;F</p>
-              <p>${description}</p>
-              <p>${humidity} humidity</p>
+            if (hour >= 12 && temperature > currentDay.temperature) {
+              highestTemperatures[dayOfWeek] = {
+                temperature,
+                condition,
+                humidity,
+                iconUrl
+              };
+            }
+          }
+          
+          // Render weather data
+          
+          const forecastMarkup = Object.keys(highestTemperatures)
+            .map((dayOfWeek) => {
+              const { temperature, condition, humidity, iconUrl } = highestTemperatures[dayOfWeek];
+              
+              return `
+                <div class="forecast-day">
+                  <h3>${dayOfWeek}</h3>
+                  <img src="${iconUrl}" alt="Weather Icon">
+                  <p> ${temperature} &deg;F</p>
+                  <p> ${condition}</p>
+                  <p> ${humidity}% humidity</p>
+                </div>
+              `;
+            })
+            .join("");
+
+          const weatherCard = document.getElementById("weather-card");
+          const weatherData = `
+            <div class="forecast-days">
+              
+              <div class="forecast-container">${forecastMarkup}</div>
             </div>
           `;
+          weatherCard.innerHTML = weatherData;
         })
-        .join("");
-
-      // Render weather data
-      const currentIcon = `https://openweathermap.org/img/w/${currentWeatherData.weather[0].icon}.png`;
-      const weatherCard = document.getElementById("weather-card");
-      const weatherData = `
-      
-        <div class="forecast-day" >
-          <h3>Today</h3>
-          <img src="${currentIcon}" alt="${currentDescription}">
-          <p>${currentTemperature} &deg;F</p>
-          <p>${currentDescription}</p>
-          <p>${currentHumidity} humidity</p>
-        </div>
-        <div class="forecast-days">
-            
-            <div class="forecast-container">${forecastMarkup}</div>
-        </div>
-      `;
-      weatherCard.innerHTML = weatherData;
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data: ", error);
+        .catch((error) => {
+          console.error("Error fetching weather data: ", error);
+        });
     });
 }
 
